25858ccbf3a4f9ca1e02fc76864cc9033b03c034
NazioLT <94365544+NazioLT@users.noreply.github.com>
2022-09-05 21:06:39 +0200

    Refactor Moving Tween

--- Runtime/Tweening.cs
+++ Runtime/Tweening.cs
@@ -4,19 +4,25 @@ namespace Nazio_LT.NTween
 {
     public static partial class Tweening
     {
+        #region Transform Tweening
+
         public static NTweener GoTo(this Transform _transform, Vector3 _end, float _duration)
         {
             Vector3 _start = _transform.position;
 
-            return new NTweener((_t) => _transform.position = Vector3.Lerp(_start, _end, _t), _duration);
+            return _transform.GoTo(_start, _end, _duration);
         }
+
+        public static NTweener GoTo(this Transform _transform, Vector3 _start, Vector3 _end, float _duration) => new NTweener((_t) => _transform.position = Vector3.Lerp(_start, _end, _t), _duration);
 
         public static NTweener Move(this Transform _transform, Vector3 _delta, float _duration)
         {
             Vector3 _start = _transform.position;
             Vector3 _end = _transform.position + _delta;
 
-            return new NTweener((_t) => _transform.position = Vector3.Lerp(_start, _end, _t), _duration);
+            return _transform.GoTo(_start, _end, _duration);
         }
+
+        #endregion
     }
 }
