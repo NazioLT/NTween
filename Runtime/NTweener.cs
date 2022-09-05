9fc91f91150573fb5ca4e66046bd4cf43ceea37e
NazioLT <94365544+NazioLT@users.noreply.github.com>
2022-09-05 18:27:57 +0200

    Stop and Pause

--- Runtime/NTweener.cs
+++ Runtime/NTweener.cs
@@ -19,12 +19,17 @@ namespace Nazio_LT.NTween
 
         private bool pingpong = false;
 
+        private bool stopped = false;
+        private bool paused = false;
+
         private async void StartTween()
         {
             await MainTween(false);
 
-            while(pingpong)
+            while (pingpong)
             {
+                if (stopped) return;
+
                 await MainTween(true);
                 await MainTween(false);
             }
@@ -35,21 +40,46 @@ namespace Nazio_LT.NTween
             float _endTime = Time.time + duration;
             while (Time.time < _endTime)
             {
+                await Task.Yield();
+
                 float _normalizedTime = NormalizedTime(_reverse, duration, _endTime);
 
-                action(_normalizedTime);
+                if (stopped) return;
+                if (Paused(ref _endTime, duration, _normalizedTime)) continue;
 
-                await Task.Yield();
+                action(_normalizedTime);
             }
         }
 
         private float NormalizedTime(bool _reverse, float _duration, float _endTime)
         {
-            float _normalizedTime = (_endTime - Time.time) / duration;
+            float _deltaTime = (_endTime - Time.time);
+            float _normalizedTime = _deltaTime / duration;
 
             if (_reverse) return _normalizedTime;
             return 1 - _normalizedTime;
         }
+
+        private bool Paused(ref float _endTime, float _duration, float _normalizedTime)
+        {
+            if (!paused) return false;
+
+            float _remainingDuration = (1 - _normalizedTime) * duration;
+            _endTime = Time.time + _remainingDuration;
+
+            return true;
+        }
+
+        #region Orders
+
+        public void Stop() => stopped = true;
+
+        public void Pause() => paused = true;
+        public void Resume() => paused = false;
+        public void SetPause(bool _value) => paused = _value;
+        public void InversePause() => paused = !paused;
+
+        #endregion
 
         #region Settings
 
